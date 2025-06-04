b3bfb1085254d3cf91ad07fe47d8865938658b4b
JHJ-lab <jhj072787@gmail.com>
2025-06-04 23:33:56 +0900

    main, calendar, recipe
    main, calendar, recipe

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
+
 # routes에서 Blueprint import
 from routes.auth_routes import auth_bp
 from routes.record_routes import record_bp
 #from routes.user_routes import user_bp
 #from routes.yolo_routes import image_bp
-#from routes.recipe_routes import recipe_bp
+from routes.recipe_routes import recipe_bp
 from routes.yolo_routes import yolo_bp
 
 app = Flask(__name__)
@@ -16,7 +17,7 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(record_bp)
 #app.register_blueprint(user_bp)
 #app.register_blueprint(image_bp)
-#app.register_blueprint(recipe_bp)
+app.register_blueprint(recipe_bp)
 app.register_blueprint(yolo_bp)
 
 if __name__ == '__main__':
