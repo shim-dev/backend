82891debcdb8069701c9b032d7ba5bd4b5913654
chaelim <dlcofla414@naver.com>
2025-06-05 00:02:20 +0900

    프로필 사진 추가

--- routes/signup/nick_routes.py
+++ routes/signup/nick_routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify
 from config import users  
 from bson import ObjectId
-from bson.errors import InvalidId
 
 nick_bp = Blueprint('nick', __name__)
 
@@ -10,6 +9,7 @@ def set_nickname():
     data = request.json
     user_id = data.get('user_id')
     nickname = data.get('nickname')
+    profile_url = data.get('profile_url')  
 
     if not user_id or not nickname:
         return jsonify({"message": "user_id와 닉네임이 필요합니다."}), 400
@@ -23,11 +23,17 @@ def set_nickname():
     except Exception:
         return jsonify({"message": "user_id 형식이 잘못되었습니다."}), 400
 
+    #  저장할 필드 구성
+    update_data = {"nickname": nickname}
+    if profile_url:
+        update_data["profile_url"] = profile_url
+
     result = users.update_one(
         {"_id": object_id},
-        {"$set": {"nickname": nickname}}
+        {"$set": update_data}
     )
+
     if result.matched_count == 0:
         return jsonify({"message": "해당 user_id를 가진 사용자가 없습니다."}), 404
 
-    return jsonify({"message": "닉네임 저장 성공!"}), 200
+    return jsonify({"message": "닉네임 및 프로필 이미지 저장 성공!"}), 200
