20034868a21687006a191fb4dae8935769750274
JHJ-lab <jhj072787@gmail.com>
2025-06-05 18:26:23 +0900

    update about containing weight file

--- routes/yolo/yolo_routes.py
+++ routes/yolo/yolo_routes.py
@@ -7,9 +7,10 @@ import os
 yolo_bp = Blueprint('yolo', __name__)
 
 # 상대 경로로 모델 로드
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # 현재 파일(yolo_routes.py)의 경로
-MODEL_PATH = os.path.join(BASE_DIR, '..', 'weights', 'best.pt')  # 상위폴더/weights/best.pt
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, '..', '..', 'weights', 'best.pt')
 model = YOLO(MODEL_PATH)
+
 
 @yolo_bp.route('/detect', methods=['POST'])
 def detect_food():
