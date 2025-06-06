4d40556c569c06c1ffbc75c32e1f2ab227669f8b
sua00 <suasua0105@gmail.com>
2025-06-06 15:25:01 +0900

    fix: resolve conflicts in app.py and config.py

--- config.py
+++ config.py
@@ -2,26 +2,28 @@ import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
-# .env 파일 로드
 load_dotenv()
 
-# 환경 변수에서 MongoDB 연결 정보 불러오기
 mongo_uri = os.getenv("MONGO_URI")
 mongo_db_name = os.getenv("MONGO_DB")
 
-# MongoDB 클라이언트 및 DB 연결
 client = MongoClient(mongo_uri)
 db = client[mongo_db_name]
 
-users = db["users"]
-health_profiles = db["health_profiles"]
-records = db["records"]
-water_records = db["water_records"]
-sleep_hours = db["sleep_hours"]
+records = db['records']
+
+water_records = db['water_records']
+sleep_hours = db['sleep_hours']
+users = db['users']
 notices_col = db["notices"]
 events_col = db["events"]
-inquiry_col = db["inquiries"]
-quit_reasons_col = db["quit_reasons"]
-images_col = db["images"]
-bookmarks = db["bookmarks"]
-recipes = db["recipes"]
+inquiry_col = db['inquiries']
+quit_reasons_col = db['quit_reasons']
+images_col = db['images']
+bookmarks = db['bookmarks']
+recipes = db['recipes']
+keywords = db['keywords']
+
+# 챎 언니 파트 #
+health_profiles = db["health_profiles"]
+# 챎 언니 파트 끝#
